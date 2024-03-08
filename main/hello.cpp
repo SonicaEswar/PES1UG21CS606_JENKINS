1c129ec83f3cd58bb0748e1e4ffcb67eca42a038
SonicaEswar <148249499+SonicaEswar@users.noreply.github.com>
2024-03-08 15:23:07 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout <<"update\n";
+  cout <<"webhook succesfully enabled\n";
   return 0;
 }
